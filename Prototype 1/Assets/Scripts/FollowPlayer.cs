c3c6f4b656adb5c9b1118d7bea07f9b250c87bd0
Gabriel Small <gabriel.s277@stu.nebo.edu>
2020-09-22 08:43:14 -0600

    Update to Code

--- Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -6,16 +6,11 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 7,-10);
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+   
     // Update is called once per frame
     void Update()
     {
+        // Makes the camera follow the movements of the player
         transform.position = player.transform.position + offset;
     }
 }
--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -10,20 +10,16 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float forwardInput;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         
-        // Moves the vehicle forward
+        // Moves the vehicle forward and back based on VerticalIput
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        
+        // Rotates the vehicle left and right based on HorizontalIput
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
         
     }
