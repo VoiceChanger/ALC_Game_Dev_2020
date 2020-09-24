2b689e67ba47d1410e8fbebab35e5129ed1c44fa
Gabriel Small <gabriel.s277@stu.nebo.edu>
2020-09-24 09:11:17 -0600

    Plane Challenge

---
+++ Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    public Vector3 offset = new Vector3(30, 10, 10);
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Makes the camera follow the plane from the side
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 30;
+    public float rotationSpeed = 50;
+    public float verticalInput = 100;
+    public float horizontalInput = 30;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical and horizontal input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // move the plane forward at a constant rate
+         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * Time.deltaTime * speed * verticalInput);
+    }
+}
