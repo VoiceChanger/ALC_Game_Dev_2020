7e30d98ece3bd91d34be51e69c715339bbdee3c8
Gabriel Small <gabriel.s277@stu.nebo.edu>
2020-10-02 08:15:52 -0600

    update

---
+++ Prototype 1/Assets/Scripts/PlanePropeller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanePropeller : MonoBehaviour
+{
+    public float spinSpeed = 50f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, spinSpeed * Time.deltaTime, 0);
+    }
+}
