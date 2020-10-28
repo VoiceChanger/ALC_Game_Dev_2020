e42958705ad85523ccd1a6316adb644bd89b421f
Gabriel Small <gabriel.s277@stu.nebo.edu>
2020-10-28 08:06:34 -0600

    Final Update

--- Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float fireRate = 1.0f; 
+    private float nextFire = 0.0f; 
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
         {
+            nextFire = Time.time + fireRate;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
